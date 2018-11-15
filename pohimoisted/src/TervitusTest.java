0b7c67ba698a8dcd356486d9afb253a2ef2a3334
riina.haidkind <riina.haidkind@khk.ee>
2018-11-15 10:03:28 +0200

    Tervitustüüp, mis võimaldab tühja tervitusobjekti luua.

--- pohimoisted/src/TervitusTest.java
+++ pohimoisted/src/TervitusTest.java
@@ -1,6 +1,8 @@
 public class TervitusTest {
     public static void main(String[] args) {
          Tervitus riinaTervitus = new Tervitus("Tere õpetaja!");
+         Tervitus vaikeTervitus = new Tervitus ();
          riinaTervitus.tervita();
+         vaikeTervitus.tervita();
     }
 }
