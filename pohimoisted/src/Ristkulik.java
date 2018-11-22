0c5a0547827c783ca44fdcd1b16a090b8aecd66c
riina.haidkind <riina.haidkind@khk.ee>
2018-11-22 12:03:48 +0200

    Ristküliku pindala arvutamise täpsus korrastatud

--- pohimoisted/src/Ristkulik.java
+++ pohimoisted/src/Ristkulik.java
@@ -15,6 +15,20 @@ public class Ristkulik extends Kujund {
         System.out.println("Ristkuliku klassi konstruktor");
     }
 
+        public void arvutaPindala(Boolean tapsus) {
+        System.out.println("Ristküliku klassi arvutaPindala() meetod");
+        this.setPindala(this.pikkus * this.laius);
+        Double pindala = this.getPindala();
+            System.out.print("Pindala = ");
+
+       if (tapsus) {
+            System.out.println(pindala);
+        } else {
+            System.out.println(Math.round(pindala));
+        }
+    }
+
+
     @Override
     public void valjastaKirjeldus() {
         System.out.println("Ristküliku klassi valjastaKirjeldus()");
@@ -22,7 +36,7 @@ public class Ristkulik extends Kujund {
         System.out.println("Pikkus = " + this.pikkus);
         System.out.println("Laius = " + this.laius);
         if (this.getNurkadeOlemasolu())
-        System.out.println("Kokku on " + this.);
+        System.out.println("Kokku on " + this.getNurkadeArv() + "nurka");
         System.out.println("Nurgad on " + this.nurk + " kraadi.");
     }
 }
