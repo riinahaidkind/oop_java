57823618e1805585b96eb9bc5f00efaef4396b8f
riina.haidkind <riina.haidkind@khk.ee>
2018-11-29 11:46:01 +0200

    Printer nüüd töötab.

--- pohimoisted/src/PrinterTest.java
+++ pohimoisted/src/PrinterTest.java
@@ -1,9 +1,13 @@
 public class PrinterTest {
     public static void main(String[] args) {
-        Printer printer1 = new Printer (50, false);
+        Printer printer1 = new Printer (150, true);
         printer1.lisaTooner(15);
         System.out.println(" Printeri tooneri tase on " + printer1.getTooneriTase() + "%");
-        printer1.lisaTooner(50);
-        System.out.println(" Printeri tooneri tase on " + printer1.getTooneriTase() + "%");
+        Integer kontrolltoo = printer1.prindi(5);
+        System.out.println("Printisin kontrolltöö jaoks " + kontrolltoo + " paberi lehte ja printeris on jäänud " +
+        printer1.getPaberPrintimiseks() + printer1.lisaPaber(50));
+        kontrolltoo = printer1.prindi(5);
+        System.out.println("Printisin kontrolltöö jaoks " + kontrolltoo + " paberi lehte ja printeris on jäänud " +
+                printer1.getPaberPrintimiseks() + " paberi lehte");
     }
 }
