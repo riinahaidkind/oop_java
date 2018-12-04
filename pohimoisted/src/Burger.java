fb378b48ad0212471342af865a3bdd025f99e5b8
riina.haidkind <riina.haidkind@khk.ee>
2018-12-04 12:44:04 +0200

    Burgeri koostisosade kirjeldus ja hinna arvutamine.

---
+++ pohimoisted/src/Burger.java
@@ -0,0 +1,27 @@
+public class Burger {
+    public String nimetus;
+    public String liha;
+    public String sai;
+    public Double hind;
+    public String lisand1;
+    public Double lisand1Hind;
+    public String lisand2;
+    public Double lisand2Hind;
+    public String lisand3;
+    public Double lisand3Hind;
+    public String lisand4;
+    public Double lisand4Hind;
+
+
+    // konstruktor
+    public Burger(String nimetus, String liha, String sai, Double hind) {
+        this.nimetus = nimetus;
+        this.liha = liha;
+        this.sai = sai;
+        this.hind = hind;
+    }
+
+    
+
+}
+
