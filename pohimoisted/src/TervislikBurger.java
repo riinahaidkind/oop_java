f6d39877af632493a852c027196126aa66dd716d
riina.haidkind <riina.haidkind@khk.ee>
2018-12-04 12:59:10 +0200

    Tervisliku burgeri koostisosade kirjeldus ja hinna arvutamine.

---
+++ pohimoisted/src/TervislikBurger.java
@@ -0,0 +1,13 @@
+public class TervislikBurger {
+    private String tervislikLisand1;
+    private Double tervislikLisand1Hind;
+    private String tervislikLisand2;
+    private Double tervislikLisand2Hind;
+
+    public TervislikBurger(String tervislikLisand1, Double tervislikLisand1Hind) {
+        this.tervislikLisand1 = tervislikLisand1;
+        this.tervislikLisand1Hind = tervislikLisand1Hind;
+    }
+
+    
+}
