fe033b8db8236c88daa992b797c63db3d6995efd
riina.haidkind <riina.haidkind@khk.ee>
2018-12-06 09:37:52 +0200

    Tervisliku burgeri konstrueerimine

--- pohimoisted/src/TervislikBurger.java
+++ pohimoisted/src/TervislikBurger.java
@@ -1,15 +1,35 @@
-public class TervislikBurger extends Burger{
-    private String tervislikLisand1;
-    private Double tervislikLisand1Hind;
-    private String tervislikLisand2;
-    private Double tervislikLisand2Hind;
+public class TervislikBurger extends Burger {
+    private String lisandTervislik1;
+    private Double lisandTervislik1Hind;
 
-    //konstruktor
+    private String lisandTervislik2;
+    private Double lisandTervislik2Hind;
 
-    public TervislikBurger(String tervislikLisand1, Double tervislikLisand1Hind) {
-        this.tervislikLisand1 = tervislikLisand1;
-        this.tervislikLisand1Hind = tervislikLisand1Hind;
+    public TervislikBurger(String liha, Double hind) {
+        super("Tervislik Burger", liha, "Teraleib", hind);
     }
 
+    public void valiTervislikLisand1(String lisandTervislik1, Double lisandTervislik1Hind){
+        this.lisandTervislik1 = lisandTervislik1;
+        this.lisandTervislik1Hind = lisandTervislik1Hind;
+    }
 
+    public void valiTervislikLisand2(String lisandTervislik2, Double lisandTervislik2Hind){
+        this.lisandTervislik2 = lisandTervislik2;
+        this.lisandTervislik2Hind = lisandTervislik2Hind;
+    }
+
+    @Override
+    public Double koostaBurger() {
+        Double hind = super.koostaBurger();
+        if(this.lisandTervislik1 != null) {
+            System.out.println("Lisatud " + this.lisandTervislik1 + " hinnaga " + this.lisandTervislik1Hind);
+            hind = hind + this.lisandTervislik1Hind;
+        }
+        if(this.lisandTervislik2 != null) {
+            System.out.println("Lisatud " + this.lisandTervislik2 + " hinnaga " + this.lisandTervislik2Hind);
+            hind = hind + this.lisandTervislik2Hind;
+        }
+        return hind;
+    }
 }
